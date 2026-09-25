16185d076f1cb97b82fa1d29fb906969b7a689cf
wliu7635 <liu03862@umn.edu>
2026-09-25 03:51:52 -0500

    Update check_mask_size.py

--- scripts/check_mask_size.py
+++ scripts/check_mask_size.py
@@ -1,1 +1,10 @@
+import nibabel as nib
 
+m = nib.load(
+r"D:\RMT\tedana_fmriprepmask\sub-01_rest_desc-adaptiveGoodSignal_mask.nii.gz"
+).get_fdata()
+
+print(
+    "voxels =",
+    (m > 0).sum()
+)
